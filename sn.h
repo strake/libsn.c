e7293bc002e0896f91147a115575603eeebbd037
Strake <strake888@gmail.com>
2013-12-16 20:22:28 -0500

    prototype sn_sub

--- sn.h
+++ sn.h
@@ -15,5 +15,6 @@ SN sn_alloc0 (ssize_t);
 
 int   sn_compar  (SN, SN);
 ulong sn_add     (SN, SN, SN);
+ulong sn_sub     (SN, SN, SN);
 void  sn_mul     (SN, SN, SN, ulong flags);
 void  sn_shlWord (SN, SN, long);

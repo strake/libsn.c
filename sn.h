22c26ac32c4f9cd6ffd3847ae40a88ebb9837a51
Strake <strake888@gmail.com>
2013-12-16 21:08:47 -0500

    no ulong in sn.h

--- sn.h
+++ sn.h
@@ -1,6 +1,6 @@
 typedef struct {
 	ssize_t nn;
-	ulong *ns; // little-endian
+	unsigned long *ns; // little-endian
 } SN;
 
 enum {
@@ -16,5 +16,5 @@ SN sn_alloc0 (ssize_t);
 int   sn_compar  (SN, SN);
 ulong sn_add     (SN, SN, SN);
 ulong sn_sub     (SN, SN, SN);
-void  sn_mul     (SN, SN, SN, ulong flags);
+void  sn_mul     (SN, SN, SN, unsigned long flags);
 void  sn_shlWord (SN, SN, long);
